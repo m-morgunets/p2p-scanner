a357dadec1ee6114d16e8bfb27a5a43cda38b0f0
m-morgunets <miha123770214@gmail.com>
2023-03-19 18:08:24 +0300

    Изменён вывод элементов в компоненте PaySub

--- frontend/src/pages/Profile/PaySub/PaySub.tsx
+++ frontend/src/pages/Profile/PaySub/PaySub.tsx
@@ -3,56 +3,81 @@ import styles from "./PaySub.module.scss";
 import PaySubItem from "./PaySubItem/PaySubItem";
 
 const PaySub = () => {
+
+	// Данные для вывода элементов
+	const arrayDataPaySub = [
+		{
+			title: "standart",
+			price: "50$",
+			numDevices: "1",
+			text: "весь необходимый функционал для работы со стандартными связками для одного человека с бюджетом до 200.000 рублей",
+			disabledBtn: false,
+			children: (
+				<>
+					- стандартный-межбиржевой
+					<br />
+					- конвертационный
+					<br />
+					<br />
+					<br />
+				</>
+			),
+		},
+		{
+			title: "pro",
+			price: "100$",
+			numDevices: "3",
+			text: "профессиональная работа с более сложными связками, включающими обнал через Гарантекс, разные валюты, страны и так далее",
+			disabledBtn: true,
+			children: (
+				<>
+					- стандартный-межбиржевой
+					<br />
+					- конвертационный
+					<br />
+					- биржи + гарантекс
+					<br />- Binance + BestChange
+				</>
+			),
+		},
+		{
+			title: "business",
+			price: "200$",
+			numDevices: "по запросу",
+			text: "решение для команд и p2p-офисов, суть которого заключается в предоставлении доступа оптом сразу нескольким сотрудникам",
+			disabledBtn: true,
+			children: (
+				<>
+					- стандартный-межбиржевой
+					<br />
+					- конвертационный
+					<br />
+					- биржи + гарантекс
+					<br />- Binance + BestChange
+				</>
+			),
+		},
+	];
+
 	return (
 		<section className={styles.section}>
 			<div className={styles.title}>
 				<p>-----------------</p> оплатить подписку <p>-----------------</p>
 			</div>
 			<div className={styles.inner}>
-				<PaySubItem
-					title="standart"
-					price="50$"
-					numDevices="1"
-					text="весь необходимый функционал для работы со стандартными связками для одного человека с бюджетом до 200.000 рублей"
-					disabledBtn={false}
-				>
-					- стандартный-межбиржевой
-					<br />
-					- конвертационный
-					<br />
-					<br />
-					<br />
-				</PaySubItem>
+				
+				{arrayDataPaySub.map((item) => (
+					<PaySubItem
+						title={item.title}
+						price={item.price}
+						numDevices={item.numDevices}
+						text={item.text}
+						disabledBtn={item.disabledBtn}
+					>
+						{item.children}
+					</PaySubItem>
+				))}
 
-				<PaySubItem
-					title="pro"
-					price="100$"
-					numDevices="3"
-					text="профессиональная работа с более сложными связками, включающими обнал через Гарантекс, разные валюты, страны и так далее"
-					disabledBtn={true}
-				>
-					- стандартный-межбиржевой
-					<br />
-					- конвертационный
-					<br />
-					- биржи + гарантекс
-					<br />- Binance + BestChange
-				</PaySubItem>
-
-				<PaySubItem
-					title="business"
-					price="200$"
-					numDevices="по запросу"
-					text="решение для команд и p2p-офисов, суть которого заключается в предоставлении доступа оптом сразу нескольким сотрудникам"
-					disabledBtn={true}
-				>
-					- стандартный-межбиржевой
-					<br />
-					- конвертационный
-					<br />
-					- биржи + гарантекс
-					<br />- Binance + BestChange
-				</PaySubItem>
 			</div>
 		</section>
 	);
